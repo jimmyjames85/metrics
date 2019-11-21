59a057cb797c7c54a412b3ea96e8cc369cac64e0
jimmyjames <james.tappe@sendgrid.com>
2019-11-20 16:54:37 -0800

    docker

--- cmd/webservice/main.go
+++ cmd/webservice/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/jimmyjames85/metrics/internal/webservice"
 
 func main() {
-	port := 8080
+	port := 5555
 	s := webservice.New(port)
 	err := s.Serve()
 	if err != nil {
